0252a21848d5c630c46e6286c6993f7cca5168e6
EugeneZ <eugenez@invenco.com>
2019-10-22 23:00:26 +1300

    Variables.js added, package.json test script modified to execute variables.js

---
+++ variables.js
@@ -0,0 +1,24 @@
+const a = 'Value a canot be changed';
+console.log(a);
+
+let b = 'Value b can be changed';
+console.log(b);
+
+b = 'Value b is changed now';
+console.log(b);
+
+let c = 154;
+
+console.log(c);
+
+let d = [ {"name":"Mike"}, 2, 'Anoteher String'];
+console.log(d);
+console.log(d[1]);
+
+let e = {"name":"John", "age":25};
+console.log(e);
+console.log(e.name);
+console.log(e.age);
+
+a = "Should be error here as const cant be reassigned";
+console.log(a);
