e6d40421313fea77d678c8c331b6ae6ea1296375
EugeneZ <eugenez@invenco.com>
2019-10-22 23:35:02 +1300

    lodash example added

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
 const users = require("./users.json");
+const _ = require('lodash');
 
 const checkIfAdult = function (user) {
     if (user.age > 17) {
@@ -6,8 +7,8 @@ const checkIfAdult = function (user) {
     } else return false
 }
 
-for (const user of users){
+_.forEachRight(users, function(user) {
     if (checkIfAdult(user)){
         console.log(`${user.name} is adult` )
     } else console.log(`${user.name} is not adult` )
-}
+});
